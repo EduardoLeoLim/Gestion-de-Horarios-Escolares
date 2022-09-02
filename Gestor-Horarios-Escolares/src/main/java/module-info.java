135a779aafba6e8400d2e0fed0bbe8e4540579d9
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-02 13:40:58 -0500

    Bibliotecas mysql-connector y sql2o agregadas

--- Gestor-Horarios-Escolares/src/main/java/module-info.java
+++ Gestor-Horarios-Escolares/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.gamma.gestorhorariosescolares {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires sql2o;
 
     opens com.gamma.gestorhorariosescolares to javafx.fxml;
     exports com.gamma.gestorhorariosescolares;
