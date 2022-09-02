3e6252df3ed02b78ddc27838dc431821ccf56991
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-09-02 13:51:04 -0500

    Módulo java.sql agregado

--- Gestor-Horarios-Escolares/src/main/java/module-info.java
+++ Gestor-Horarios-Escolares/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.gamma.gestorhorariosescolares {
     requires javafx.controls;
     requires javafx.fxml;
     requires sql2o;
+    requires java.sql;
 
     opens com.gamma.gestorhorariosescolares to javafx.fxml;
     exports com.gamma.gestorhorariosescolares;
