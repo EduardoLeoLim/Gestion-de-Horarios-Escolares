8f2374b05b1801d20bed6161390747ed7cf44ac3
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-01 09:07:50 -0500

    Stage personalizado creado

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/App.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/App.java
@@ -1,21 +1,13 @@
 package com.gamma.gestorhorariosescolares;
 
-import com.gamma.gestorhorariosescolares.infrastructure.user.viewcontrollers.LoginController;
+import com.gamma.gestorhorariosescolares.infrastructure.user.stages.LoginStage;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/user/Login.fxml"));
-        fxmlLoader.setController(new LoginController());
-        Scene scene = new Scene(fxmlLoader.load());
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        new LoginStage().show();
     }
 
     public static void main(String[] args) {
