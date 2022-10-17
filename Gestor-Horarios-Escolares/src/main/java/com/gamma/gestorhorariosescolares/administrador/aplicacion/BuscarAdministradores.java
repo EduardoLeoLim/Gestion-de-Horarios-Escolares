f74a00ed60f127ccf3a47f40f3f9c99e65f5c408
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-17 10:31:50 -0500

    feat(administrador): caso de uso buscar administradores

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/administrador/aplicacion/BuscarAdministradores.java
@@ -0,0 +1,28 @@
+package com.gamma.gestorhorariosescolares.administrador.aplicacion;
+
+import com.gamma.gestorhorariosescolares.administrador.aplicacion.buscar.BuscadorAdministrador;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.buscar.BuscadorUsuario;
+
+import java.util.stream.Collectors;
+
+public class BuscarAdministradores {
+
+    private BuscadorAdministrador buscadorAdministrador;
+    private BuscadorUsuario buscadorUsuario;
+
+    public BuscarAdministradores(BuscadorAdministrador buscadorAdministrador, BuscadorUsuario buscadorUsuario) {
+        this.buscadorAdministrador = buscadorAdministrador;
+        this.buscadorUsuario = buscadorUsuario;
+    }
+
+    public AdministradoresData buscarTodos() {
+        var listaAdministradores =  buscadorAdministrador.buscar();
+        var listaAdministradoresData = listaAdministradores.stream().map(administrador -> {
+            var usuario = buscadorUsuario.igual("id", String.valueOf(administrador.idUsuario())).buscar().get(0);
+            return AdministradorData.fromAggregate(administrador, usuario);
+        });
+
+        return new AdministradoresData(listaAdministradoresData.collect(Collectors.toList()));
+    }
+
+}
