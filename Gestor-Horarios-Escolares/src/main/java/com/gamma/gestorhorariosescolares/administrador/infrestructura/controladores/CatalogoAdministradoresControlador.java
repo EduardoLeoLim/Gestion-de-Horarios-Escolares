23a74d74ccc8bbb956bd751249a07a379bf0868b
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-17 10:34:58 -0500

    feat(administrador): tableview muestra datos de administradores

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/administrador/infrestructura/controladores/CatalogoAdministradoresControlador.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/administrador/infrestructura/controladores/CatalogoAdministradoresControlador.java
@@ -1,10 +1,17 @@
 package com.gamma.gestorhorariosescolares.administrador.infrestructura.controladores;
 
+import com.gamma.gestorhorariosescolares.administrador.aplicacion.AdministradorData;
 import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.Temporizador;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.UsuarioData;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CatalogoAdministradoresControlador {
     private final Stage stage;
@@ -15,7 +22,7 @@ public class CatalogoAdministradoresControlador {
     private TextField txtBuscar;
 
     @FXML
-    private TableView tablaAdministradores;
+    private TableView<AdministradorData> tablaAdministradores;
 
     public CatalogoAdministradoresControlador(Stage stage) {
         if (stage == null)
@@ -37,10 +44,97 @@ public class CatalogoAdministradoresControlador {
                 return;
             temporizadorBusqueda.reiniciar();
         });
+
+        //Configuracion de la tabla
+        inicializarTabla();
+    }
+
+    private void inicializarTabla() {
+        //Borrar después
+        var u = new UsuarioData(1, "correo", "clave", "admin");
+        var a = new AdministradorData(1, "noPErsonal", "angel", "martinez", "leo lim", true, u);
+        List<AdministradorData> lista = new ArrayList<>();
+        lista.add(a);
+        ObservableList<AdministradorData> coleccion = FXCollections.observableArrayList(lista);
+        tablaAdministradores.setItems(coleccion);
+        var u2 = new UsuarioData(1, "cofdfdrreo", "clafdfve", "adfdfmin");
+        var a2 = new AdministradorData(1, "noPErfdfdsonal", "anfdfdgel", "marfdfdtinez", "leofdfd lim", true, u2);
+        tablaAdministradores.setItems(coleccion);
+        coleccion.addAll(new ArrayList<AdministradorData>());
+        //Borrar después
+
+        TableColumn<AdministradorData, String> columnaNoPersonal = new TableColumn<>("No. Personal");
+        columnaNoPersonal.setCellValueFactory(ft -> new SimpleStringProperty(ft.getValue().noPersonal()));
+        columnaNoPersonal.setMinWidth(150);
+
+        TableColumn<AdministradorData, String> columnaNombre = new TableColumn<>("Nombre");
+        columnaNombre.setCellValueFactory(ft -> new SimpleStringProperty(ft.getValue().nombre()));
+        columnaNombre.setMinWidth(150);
+
+        TableColumn<AdministradorData, String> columnaApellidoPaterno = new TableColumn<>("Apellido Paterno");
+        columnaApellidoPaterno.setCellValueFactory(ft -> new SimpleStringProperty(ft.getValue().apellidoPaterno()));
+        columnaApellidoPaterno.setMinWidth(150);
+
+        TableColumn<AdministradorData, String> columnaApellidoMaterno = new TableColumn<>("Apellido Materno");
+        columnaApellidoMaterno.setCellValueFactory(ft -> new SimpleStringProperty(ft.getValue().apellidoMaterno()));
+
+        TableColumn<AdministradorData, String> columnaCorreoElectronico = new TableColumn<>("Correo Electrónico");
+        columnaCorreoElectronico.setCellValueFactory(ft -> new SimpleStringProperty(ft.getValue().usuario().correoElectronico()));
+        columnaCorreoElectronico.setMinWidth(200);
+
+        TableColumn<AdministradorData, String> columnaEditar = new TableColumn<>("");
+        columnaEditar.setCellFactory(ft -> new TableCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(null);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    AdministradorData administrador = getTableView().getItems().get(getIndex());
+                    Button botonEditar = new Button("Editar");
+                    botonEditar.getStyleClass().addAll("b","btn-success");
+                    botonEditar.setOnAction(event -> editarAdministrador(administrador));
+                    setGraphic(botonEditar);
+                }
+            }
+        });
+
+        TableColumn<AdministradorData, String> columnaEliminar = new TableColumn<>("");
+        columnaEliminar.setCellFactory(ft -> new TableCell<>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                setText(null);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    AdministradorData administrador = getTableView().getItems().get(getIndex());
+                    Button botonEliminar = new Button("Eliminar");
+                    botonEliminar.getStyleClass().addAll("b","btn-danger");
+                    botonEliminar.setOnAction(event -> eliminarAdministrador(administrador));
+                    setGraphic(botonEliminar);
+                }
+            }
+        });
+
+        tablaAdministradores.getColumns().addAll(columnaNoPersonal, columnaNombre, columnaApellidoPaterno,
+                columnaApellidoMaterno, columnaCorreoElectronico, columnaEditar, columnaEliminar);
     }
 
     private void buscarAdministradores(String criterioBuscqueda) {
         System.out.println("Busqueda de administradores por criterio: '" + criterioBuscqueda + "'");
+    }
+
+    public void editarAdministrador(AdministradorData administrador) {
+//        var a = new Stage();
+//        a.initModality(Modality.WINDOW_MODAL);
+//        a.initOwner(stage);
+//        a.showAndWait();
+    }
+
+    public void eliminarAdministrador(AdministradorData administrador) {
+
     }
 
     public void liberarRecursos() {
