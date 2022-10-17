d2fba1a8665ff2ca68b12733d3bdebb96b3707bc
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-16 20:24:24 -0500

    perf(administrador): busqueda mejorada
    1. Se borran los espacios al principio y al final de la cadena de búsqueda antes de hacerla.
    2. La búsqueda solo se realiza cuando se modifica la cadena de texto.

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/administrador/infrestructura/controladores/CatalogoAdministradoresControlador.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/administrador/infrestructura/controladores/CatalogoAdministradoresControlador.java
@@ -33,14 +33,16 @@ public class CatalogoAdministradoresControlador {
         //Configuración de busqueda
         temporizadorBusqueda = new Temporizador(1, (temporizador) -> {
             //La funcion es llamada cuando se agota el tiempo
-            buscarAdministradores(txtBuscar.getText());
+            buscarAdministradores(txtBuscar.getText().trim());
         });
-        txtBuscar.setOnKeyReleased(event -> {
+        txtBuscar.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (oldValue.equals(newValue))//No se realiza la busqueda cuando se presionan teclas que no modifican la cadena de búsqueda.
+                return;
             temporizadorBusqueda.reiniciar();
         });
     }
 
     private void buscarAdministradores(String criterioBuscqueda) {
-        System.out.println("Busqueda de administradores por criterio: " + criterioBuscqueda);
+        System.out.println("Busqueda de administradores por criterio: '" + criterioBuscqueda + "'");
     }
 }
