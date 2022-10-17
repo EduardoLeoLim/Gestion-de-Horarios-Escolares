dbc3b55bdf89d666af7e372052e0f495c4e53f3f
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-16 23:28:52 -0500

    feat(usuario): se agrega campo tipo a la clase Usuario

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/usuario/aplicacion/registrar/ServicioRegistradorUsuario.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/usuario/aplicacion/registrar/ServicioRegistradorUsuario.java
@@ -1,5 +1,5 @@
 package com.gamma.gestorhorariosescolares.usuario.aplicacion.registrar;
 
 public interface ServicioRegistradorUsuario {
-    int registrar(String correoElectronico, String claveAcceso);
+    int registrar(String correoElectronico, String claveAcceso, String tipo);
 }
