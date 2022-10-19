7395d8c5448567d4f84b13d14fb4e3b1ba5408d9
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-18 20:29:05 -0500

    feat(salon): clase Salon definida

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/salon/dominio/Salon.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/salon/dominio/Salon.java
@@ -1,4 +1,53 @@
 package com.gamma.gestorhorariosescolares.salon.dominio;
 
 public class Salon {
+    private final int id;
+    private final String clave;
+    private final String nombre;
+    private final int capacidad;
+    private boolean estatus;
+
+    public Salon(int id, String clave, String nombre, int capacidad, boolean estatus) {
+        this.id = id;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.estatus = estatus;
+    }
+
+    public Salon(String clave, String nombre, int capacidad) {
+        id = 0;
+        estatus = true;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String clave() {
+        return clave;
+    }
+
+    public String nombre() {
+        return nombre;
+    }
+
+    public int capacidad() {
+        return capacidad;
+    }
+
+    public void habilitar() {
+        estatus = true;
+    }
+
+    public void deshabilitar() {
+        estatus = false;
+    }
+
+    public boolean estatus() {
+        return estatus;
+    }
 }
