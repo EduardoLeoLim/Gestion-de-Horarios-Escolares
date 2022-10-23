4f7df8a83171444ac0c28125633753e712e62a17
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-22 21:58:40 -0500

    feat(usuario): actualizador de usuarios agreagado

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/usuario/aplicacion/actualizar/ActualizadorUsuario.java
@@ -0,0 +1,19 @@
+package com.gamma.gestorhorariosescolares.usuario.aplicacion.actualizar;
+
+import com.gamma.gestorhorariosescolares.usuario.dominio.Usuario;
+import com.gamma.gestorhorariosescolares.usuario.dominio.UsuarioRepositorio;
+
+public class ActualizadorUsuario implements ServicioActualizadorUsuario{
+
+    private final UsuarioRepositorio repositorio;
+
+    public ActualizadorUsuario(UsuarioRepositorio repositorio){
+        this.repositorio = repositorio;
+    }
+
+    @Override
+    public void actualizar(Usuario usuario) {
+        //validar formato datos antes de actualizar
+        repositorio.actualizar(usuario);
+    }
+}
