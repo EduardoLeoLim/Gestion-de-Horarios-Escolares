c10536d8e8b469e84c1bfcdfd9154bb64825a0a1
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-23 23:12:24 -0500

    feat(edificio): buscador edificio

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/edificio/aplicacion/buscar/BuscadorEdificio.java
@@ -0,0 +1,110 @@
+package com.gamma.gestorhorariosescolares.edificio.aplicacion.buscar;
+
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Filter;
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Filters;
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Order;
+import com.gamma.gestorhorariosescolares.edificio.dominio.Edificio;
+import com.gamma.gestorhorariosescolares.edificio.dominio.EdificioRepositorio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BuscadorEdificio implements ServicioBuscador<Edificio> {
+
+    private final EdificioRepositorio repositorio;
+    private final List<Filter> filtros;
+    private Order ordenador;
+    private Optional<Integer> intervalo;
+    private Optional<Integer> limite;
+
+    public BuscadorEdificio(EdificioRepositorio repositorio) {
+        this.repositorio = repositorio;
+        filtros = new ArrayList<>();
+        ordenador = Order.none();
+        intervalo = Optional.empty();
+        limite = Optional.empty();
+    }
+
+    @Override
+    public BuscadorEdificio igual(String campo, String valor) {
+        filtros.add(Filter.create(campo, "=", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio diferente(String campo, String valor) {
+        filtros.add(Filter.create(campo, "!=", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio mayorQue(String campo, String valor) {
+        filtros.add(Filter.create(campo, ">", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio mayorIgualQue(String campo, String valor) {
+        filtros.add(Filter.create(campo, ">=", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio menorQue(String campo, String valor) {
+        filtros.add(Filter.create(campo, "<", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio menorIgualQue(String campo, String valor) {
+        filtros.add(Filter.create(campo, "<=", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio contiene(String campo, String valor) {
+        filtros.add(Filter.create(campo, "CONTAINS", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio noContiene(String campo, String valor) {
+        filtros.add(Filter.create(campo, "NOT_CONTAINS", valor));
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio ordenarAscendente(String campo) {
+        ordenador = Order.asc(campo);
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio ordenarDescendente(String campo) {
+        ordenador = Order.desc(campo);
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio intervalo(int intervalo) {
+        this.intervalo = Optional.of(intervalo);
+        return this;
+    }
+
+    @Override
+    public BuscadorEdificio limite(int limite) {
+        this.limite = Optional.of(limite);
+        return this;
+    }
+
+    @Override
+    public List<Edificio> buscar() {
+        Criteria criterio = new Criteria(new Filters(filtros), ordenador, intervalo, limite);
+        List<Edificio> listaEdificios = repositorio.buscar(criterio);
+        filtros.clear();
+        return listaEdificios;
+    }
+}
