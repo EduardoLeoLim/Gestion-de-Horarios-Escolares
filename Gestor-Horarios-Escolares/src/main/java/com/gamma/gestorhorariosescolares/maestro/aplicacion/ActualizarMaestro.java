8e931346b11d56a7c84a8ab1b94e1e4407d348f7
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-25 18:36:12 -0500

    feat(maestro): registrar y actualizar maestros

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/aplicacion/ActualizarMaestro.java
@@ -0,0 +1,75 @@
+package com.gamma.gestorhorariosescolares.maestro.aplicacion;
+
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.excepciones.NoPersonalDuplicadoException;
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.excepciones.RecursoNoEncontradoException;
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.maestro.aplicacion.actualizar.ServicioActualizadorMaestro;
+import com.gamma.gestorhorariosescolares.maestro.dominio.Maestro;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.UsuarioData;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.actualizar.ServicioActualizadorUsuario;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.excepciones.UsuarioDuplicadoException;
+import com.gamma.gestorhorariosescolares.usuario.dominio.Usuario;
+
+import java.util.List;
+
+public class ActualizarMaestro {
+
+    private final ServicioBuscador<Maestro> buscadorMaestro;
+    private final ServicioActualizadorMaestro actualizadorMaestro;
+    private final ServicioBuscador<Usuario> buscadorUsuario;
+    private final ServicioActualizadorUsuario actualizadorUsuario;
+
+    public ActualizarMaestro(ServicioBuscador<Maestro> buscadorMaestro, ServicioActualizadorMaestro actualizadorMaestro,
+                             ServicioBuscador<Usuario> buscadorUsuario, ServicioActualizadorUsuario actualizadorUsuario) {
+        this.buscadorMaestro = buscadorMaestro;
+        this.actualizadorMaestro = actualizadorMaestro;
+        this.buscadorUsuario = buscadorUsuario;
+        this.actualizadorUsuario = actualizadorUsuario;
+    }
+
+    public void actualizar(MaestroData maestroData) throws RecursoNoEncontradoException, NoPersonalDuplicadoException, UsuarioDuplicadoException {
+        if (maestroData == null)
+            throw new NullPointerException();
+
+        List<Maestro> listaBusquedaMaestro;
+
+        //Validar si el maestro esta registrado
+        listaBusquedaMaestro = buscadorMaestro
+                .igual("id", String.valueOf(maestroData.id()))
+                .igual("idUsuario", String.valueOf(maestroData.usuario().id()))
+                .buscar();
+        if (listaBusquedaMaestro.isEmpty())
+            throw new RecursoNoEncontradoException("El maestro no se encuentra registrado en el sistema");
+
+        //Validar disponibilidad de número de personal
+        listaBusquedaMaestro = buscadorMaestro
+                .igual("noPersonal", maestroData.noPersonal())
+                .diferente("id", String.valueOf(maestroData.id()))
+                .buscar();
+        if (!listaBusquedaMaestro.isEmpty())
+            throw new NoPersonalDuplicadoException("Ya hay un maestro registrado con el número de personal "
+                    + maestroData.noPersonal());
+
+        //El correo electrónico no es ocupado por otro usuario
+        List<Usuario> listaBusquedaUsuario = buscadorUsuario
+                .igual("correoElectronico", maestroData.usuario().correoElectronico())
+                .diferente("id", String.valueOf(maestroData.usuario().id()))
+                .buscar();
+        if (listaBusquedaUsuario.size() > 0)
+            throw new UsuarioDuplicadoException("Ya hay un usario registrado con ese correo electrónico "
+                    + maestroData.usuario().correoElectronico());
+
+        //Preparando datos para actualizar
+        Maestro maestro = new Maestro(maestroData.id(), maestroData.estatus(), maestroData.noPersonal(),
+                maestroData.nombre(), maestroData.apellidoPaterno(), maestroData.apellidoMaterno(),
+                maestroData.usuario().id());
+
+        UsuarioData usuarioData = maestroData.usuario();
+        Usuario usuario = new Usuario(usuarioData.id(), usuarioData.telefono(), usuarioData.correoElectronico(),
+                usuarioData.claveAcceso(), usuarioData.tipo());
+
+        //Actualizando datos
+        actualizadorUsuario.actualizar(usuario);
+        actualizadorMaestro.actualizar(maestro);
+    }
+}
--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/aplicacion/RegistrarMaestro.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/aplicacion/RegistrarMaestro.java
@@ -1,4 +1,52 @@
 package com.gamma.gestorhorariosescolares.maestro.aplicacion;
 
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.excepciones.NoPersonalDuplicadoException;
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.maestro.aplicacion.registrar.ServicioRegistradorMaestro;
+import com.gamma.gestorhorariosescolares.maestro.dominio.Maestro;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.excepciones.UsuarioDuplicadoException;
+import com.gamma.gestorhorariosescolares.usuario.aplicacion.registrar.ServicioRegistradorUsuario;
+import com.gamma.gestorhorariosescolares.usuario.dominio.Usuario;
+
+import java.util.List;
+
 public class RegistrarMaestro {
+
+    private final ServicioBuscador<Maestro> buscadorMaestro;
+    private final ServicioRegistradorMaestro registradorMaestro;
+    private final ServicioBuscador<Usuario> buscadorUsuario;
+    private final ServicioRegistradorUsuario registradorUsuario;
+
+    public RegistrarMaestro(ServicioBuscador<Maestro> buscadorMaestro, ServicioRegistradorMaestro registradorMaestro,
+                            ServicioBuscador<Usuario> buscadorUsuario, ServicioRegistradorUsuario registradorUsuario) {
+        this.buscadorMaestro = buscadorMaestro;
+        this.registradorMaestro = registradorMaestro;
+        this.buscadorUsuario = buscadorUsuario;
+        this.registradorUsuario = registradorUsuario;
+    }
+
+    public void registrar(String noPersonal, String nombre, String apellidoPaterno, String apellidoMaterno,
+                          String telefono, String correoElectronico, String claveAcceso)
+            throws NoPersonalDuplicadoException, UsuarioDuplicadoException {
+        List<Maestro> listaBusquedaMaestro;
+        List<Usuario> listaBusquedaUsuario;
+
+        //¿Hay un maestro registrado con el número de personal?
+        listaBusquedaMaestro = buscadorMaestro
+                .igual("noPersonal", noPersonal)
+                .buscar();
+        if (!listaBusquedaMaestro.isEmpty())
+            throw new NoPersonalDuplicadoException("Ya hay un maestro registrado con el número de personal " + noPersonal);
+
+        //¿Hay un usuario registrado con el correo electrónico?
+        listaBusquedaUsuario = buscadorUsuario
+                .igual("correoElectronico", correoElectronico)
+                .buscar();
+        if (!listaBusquedaUsuario.isEmpty())
+            throw new UsuarioDuplicadoException("Ya hay un usuario registrado con el correo electrónico "
+                    + correoElectronico);
+
+        int idUsuario = registradorUsuario.registrar(telefono, correoElectronico, claveAcceso, "Maestro");
+        registradorMaestro.registrar(noPersonal, nombre, apellidoPaterno, apellidoMaterno, idUsuario);
+    }
 }
