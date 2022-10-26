d6904542727d4176c474844f8d979ff5611b794c
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-25 20:45:30 -0500

    feat(evaluacion): EvaluacionData definida

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/EvaluacionData.java
@@ -0,0 +1,18 @@
+package com.gamma.gestorhorariosescolares.evaluacion;
+
+import com.gamma.gestorhorariosescolares.alumno.aplicacion.AlumnoData;
+import com.gamma.gestorhorariosescolares.materia.aplicacion.MateriaData;
+
+public class EvaluacionData {
+    private final Integer id;
+    private final String evaluacion;
+    private final AlumnoData alumno;
+    private final MateriaData materia;
+
+    public EvaluacionData(Integer id, String evaluacion, AlumnoData alumno, MateriaData materia) {
+        this.id = id;
+        this.evaluacion = evaluacion;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+}
