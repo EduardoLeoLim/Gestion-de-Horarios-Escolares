a323a8956c1520a3b1c28e666af5baed19ce3088
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-28 22:50:43 -0500

    feat: parser de criteria para Sql2o
    Se puede obtener la query para Sql2o.
    Se puede obtener la lista de parámetros para Sql2o.

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/compartido/infrestructura/utilerias/MySqlCriteriaParser.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/compartido/infrestructura/utilerias/MySqlCriteriaParser.java
@@ -1,37 +1,47 @@
 package com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias;
 
-import com.gamma.gestorhorariosescolares.compartido.dominio.CriteriaParser;
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Filter;
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.FilterOperator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
-public class MySqlCriteriaParser implements CriteriaParser {
+public class MySqlCriteriaParser {
 
     private final String tabla;
     private final List<String> columnas;
-    private Criteria criteria;
+    private final List<Filter> filtrosObligatorios;
+    private final List<Filter> filtrosOpcionales;
+    private final Criteria criteria;
 
-    public MySqlCriteriaParser(String tabla) throws Exception {
+    public MySqlCriteriaParser(String tabla, Criteria criteria) {
         if (tabla == null)
             throw new NullPointerException();
-        if (tabla.isBlank())
-            throw new Exception();
 
         this.tabla = tabla;
+        this.criteria = criteria;
+
         columnas = new ArrayList<>();
+        filtrosObligatorios = new ArrayList<>();
+        this.filtrosObligatorios.addAll(criteria.filters().filters().stream()
+                .filter(filter -> filter.isObligatory())
+                .collect(Collectors.toList())
+        );
+
+        filtrosOpcionales = new ArrayList<>();
+        this.filtrosOpcionales.addAll(criteria.filters().filters().stream()
+                .filter(filter -> !filter.isObligatory())
+                .collect(Collectors.toList())
+        );
     }
 
 
     public void agregarCampos(String... columnas) {
         this.columnas.addAll(List.of(columnas));
-    }
-
-    public void agregarCriteria(Criteria criteria) {
-        this.criteria = criteria;
     }
 
     public String generarConsulta() {
@@ -40,6 +50,19 @@ public class MySqlCriteriaParser implements CriteriaParser {
         if (!condiciones.isEmpty()) {
             consulta += " WHERE " + condiciones;
         }
+
+        consulta += ";";
+
+        return consulta;
+    }
+
+    public String generarConsultaSql2o() {
+        String consulta = "SELECT " + generarColumnas() + " FROM " + tabla;
+        String condiciones = generarCondicionesSql2o();
+        if (!condiciones.isEmpty()) {
+            consulta += " WHERE " + condiciones;
+        }
+
 
         consulta += ";";
 
@@ -56,26 +79,55 @@ public class MySqlCriteriaParser implements CriteriaParser {
     private String generarCondiciones() {
         List<String> condiciones = new ArrayList<>();
 
-        List<Filter> filtrosObligatorios = criteria.filters().filters().stream().filter(filter -> filter.isObligatory())
-                .collect(Collectors.toList());
-        List<Filter> filtrosOpcionales = criteria.filters().filters().stream().filter(filter -> !filter.isObligatory())
-                .collect(Collectors.toList());
 
         if (!filtrosObligatorios.isEmpty()) {
             String condicionesObligatorias = filtrosObligatorios.stream().map(filter -> generarCondicion(filter))
-                    .collect(Collectors.joining(" AND ", "(",")"));
+                    .collect(Collectors.joining(" AND ", "(", ")"));
             condiciones.add(condicionesObligatorias);
         }
 
         if (!filtrosOpcionales.isEmpty()) {
             String condicionesOpcionales = filtrosOpcionales.stream().map(filter -> generarCondicion(filter))
-                    .collect(Collectors.joining(" OR ", "(",")"));
+                    .collect(Collectors.joining(" OR ", "(", ")"));
             condiciones.add(condicionesOpcionales);
         }
 
         String cadenaCondiciones = "";
         if (!condiciones.isEmpty())
-            cadenaCondiciones = condiciones.stream().collect(Collectors.joining(") AND (", "(", ")"));
+            cadenaCondiciones = condiciones.stream().collect(Collectors.joining(" AND ", "(", ")"));
+
+        return cadenaCondiciones;
+    }
+
+    private String generarCondicionesSql2o() {
+        List<String> condiciones = new ArrayList<>();
+        int numeroFiltro = 0;
+
+        if (!filtrosObligatorios.isEmpty()) {
+            List<String> listaCondiciones = new ArrayList<>();
+            for (Filter filtro : filtrosObligatorios) {
+                numeroFiltro += 1;
+                listaCondiciones.add(generarCondicionSql2o(filtro, numeroFiltro));
+            }
+            String condicionesObligatorias = listaCondiciones.stream()
+                    .collect(Collectors.joining(" AND ", "(", ")"));
+            condiciones.add(condicionesObligatorias);
+        }
+
+        if (!filtrosOpcionales.isEmpty()) {
+            List<String> listaCondiciones = new ArrayList<>();
+            for (Filter filtro : filtrosOpcionales) {
+                numeroFiltro += 1;
+                listaCondiciones.add(generarCondicionSql2o(filtro, numeroFiltro));
+            }
+            String condicionesOpcionales = listaCondiciones.stream()
+                    .collect(Collectors.joining(" OR ", "(", ")"));
+            condiciones.add(condicionesOpcionales);
+        }
+
+        String cadenaCondiciones = "";
+        if (!condiciones.isEmpty())
+            cadenaCondiciones = condiciones.stream().collect(Collectors.joining(" AND ", "(", ")"));
 
         return cadenaCondiciones;
     }
@@ -104,5 +156,62 @@ public class MySqlCriteriaParser implements CriteriaParser {
         }
 
         return condicion;
+    }
+
+    private String generarCondicionSql2o(Filter filtro, int numeroParamentro) {
+        FilterOperator operador = filtro.operator();
+
+        String condicion = "";
+        condicion += filtro.field().value();
+        switch (operador) {
+            case EQUAL -> condicion += " = ";
+            case NOT_EQUAL -> condicion += " != ";
+            case GT -> condicion += " > ";
+            case GTE -> condicion += " >= ";
+            case LT -> condicion += " < ";
+            case LTE -> condicion += " <= ";
+            case CONTAINS -> condicion += " LIKE ";
+            case NOT_CONTAINS -> condicion += " NOT LIKE ";
+            default -> condicion += "";
+        }
+
+        condicion += ":parametro" + numeroParamentro;
+
+        return condicion;
+    }
+
+    public List<Map<Integer, String>> generarParametrosSql2o() {
+        List<Map<Integer, String>> parametros = new ArrayList<>();
+        int numeroParametro = 0;
+        for (Filter filtro : filtrosObligatorios) {
+            numeroParametro += 1;
+            Map<Integer, String> parametro = new HashMap<>();
+            parametro.put(0, "parametro" + numeroParametro);
+            if (filtro.operator() == FilterOperator.CONTAINS || filtro.operator() == FilterOperator.NOT_CONTAINS) {
+                String valor = "%" + filtro.value().value() + "%";
+                parametro.put(1, valor);
+            } else {
+                parametro.put(1, filtro.value().value());
+            }
+
+
+
+            parametros.add(parametro);
+        }
+
+        for (Filter filtro : filtrosOpcionales) {
+            numeroParametro += 1;
+            Map<Integer, String> parametro = new HashMap<>();
+            parametro.put(0, "parametro" + numeroParametro);
+            if (filtro.operator() == FilterOperator.CONTAINS || filtro.operator() == FilterOperator.NOT_CONTAINS) {
+                String valor = "%" + filtro.value().value() + "%";
+                parametro.put(1, valor);
+            } else {
+                parametro.put(1, filtro.value().value());
+            }
+            parametros.add(parametro);
+        }
+
+        return parametros;
     }
 }
