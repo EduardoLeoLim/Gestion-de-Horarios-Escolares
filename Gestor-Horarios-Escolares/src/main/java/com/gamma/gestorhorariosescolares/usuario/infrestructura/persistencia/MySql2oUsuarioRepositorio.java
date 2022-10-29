2ccb4141d05410e57305553e6e50e6ad10895263
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-28 23:32:56 -0500

    feat(usuario): buscar usuarios con UsuarioRepositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/usuario/infrestructura/persistencia/MySql2oUsuarioRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/usuario/infrestructura/persistencia/MySql2oUsuarioRepositorio.java
@@ -1,26 +1,56 @@
 package com.gamma.gestorhorariosescolares.usuario.infrestructura.persistencia;
 
+import com.gamma.gestorhorariosescolares.administrador.dominio.Administrador;
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.MySqlCriteriaParser;
 import com.gamma.gestorhorariosescolares.usuario.dominio.Usuario;
 import com.gamma.gestorhorariosescolares.usuario.dominio.UsuarioRepositorio;
 import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.data.Row;
+import org.sql2o.data.Table;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MySql2oUsuarioRepositorio implements UsuarioRepositorio {
 
-    private final Connection connection;
+    private final Connection conexion;
 
-    public MySql2oUsuarioRepositorio(Connection connection) {
-        this.connection = connection;
+    public MySql2oUsuarioRepositorio(Connection conexion) {
+        this.conexion = conexion;
     }
 
     @Override
     public List<Usuario> buscar(Criteria criterio) {
         List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario(1,"1456878654","atorre2012@gmail.com","acceso", "Secretario");
-        usuarios.add(usuario);
+
+        MySqlCriteriaParser conversorCriteria = new MySqlCriteriaParser("usuario", criterio);
+        conversorCriteria.agregarCampos("id", "correoElectronico", "claveAcceso");
+        String consulta = conversorCriteria.generarConsultaSql2o();
+        List<Map<Integer, String>> parametros = conversorCriteria.generarParametrosSql2o();
+
+        Query query = conexion.createQuery(consulta);
+        System.out.println(consulta);
+
+        parametros.forEach(parametro -> {
+            query.addParameter(parametro.get(0), parametro.get(1));
+        });
+
+        Table tabla = query.executeAndFetchTable();
+        List<Row> filas = tabla.rows();
+
+        filas.forEach(fila -> {
+            int id = fila.getInteger("id");
+            String telefono = fila.getString("telefono");
+            String correoElectronico = fila.getString("correoElectronico");
+            String claveAcceso = fila.getString("claveAcceso");
+            String tipo = fila.getString("Administrador");
+            Usuario usuario = new Usuario(id, telefono, correoElectronico, claveAcceso, tipo);
+            usuarios.add(usuario);
+        });
+
         return usuarios;
     }
 
