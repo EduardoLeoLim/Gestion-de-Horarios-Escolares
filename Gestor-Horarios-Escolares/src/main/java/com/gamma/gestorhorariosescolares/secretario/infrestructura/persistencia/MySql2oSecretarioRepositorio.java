a8a2ea0c67e5ce56f44561a9d9571cf8e8477430
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-29 13:36:22 -0500

    feat(secretario): esqueleto de MySql2oSecretarioRepositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/secretario/infrestructura/persistencia/MySql2oSecretarioRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/secretario/infrestructura/persistencia/MySql2oSecretarioRepositorio.java
@@ -1,4 +1,32 @@
 package com.gamma.gestorhorariosescolares.secretario.infrestructura.persistencia;
 
-public class MySql2oSecretarioRepositorio {
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.secretario.dominio.Secretario;
+import com.gamma.gestorhorariosescolares.secretario.dominio.SecretarioRepositorio;
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class MySql2oSecretarioRepositorio implements SecretarioRepositorio {
+
+    private final Connection conexion;
+
+    public MySql2oSecretarioRepositorio(Connection conexion) {
+        this.conexion = conexion;
+    }
+
+    @Override
+    public List<Secretario> buscar(Criteria criterio) {
+        return null;
+    }
+
+    @Override
+    public int registrar(Secretario secretario) {
+        return 0;
+    }
+
+    @Override
+    public void actualizar(Secretario secretario) {
+
+    }
 }
