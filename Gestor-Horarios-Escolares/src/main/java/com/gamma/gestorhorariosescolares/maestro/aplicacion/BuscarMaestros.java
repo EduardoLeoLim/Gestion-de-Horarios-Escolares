0b8e6c06794db4e24c7369a57da97d51980c6751
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-10-29 19:25:12 -0500

    feat(maestro): buscar maestros
    Tipos de búsquedas:
    1. Buscar todos
    2. Buscar solo habilitados
    3. Buscar por criterio de búsqueda

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/aplicacion/BuscarMaestros.java
@@ -0,0 +1,63 @@
+package com.gamma.gestorhorariosescolares.maestro.aplicacion;
+
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.maestro.dominio.Maestro;
+import com.gamma.gestorhorariosescolares.usuario.dominio.Usuario;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BuscarMaestros {
+
+    private final ServicioBuscador<Maestro> buscadorMaestro;
+    private final ServicioBuscador<Usuario> buscadorUsuario;
+
+    public BuscarMaestros(ServicioBuscador<Maestro> buscadorMaestro, ServicioBuscador<Usuario> buscadorUsuario) {
+        this.buscadorMaestro = buscadorMaestro;
+        this.buscadorUsuario = buscadorUsuario;
+    }
+
+    public MaestrosData buscarTodos() {
+        List<Maestro> listaMaestros = buscadorMaestro
+                .ordenarAscendente("noPersonal")
+                .buscar();
+
+        List<MaestroData> listaMaestrosData = listaMaestros.stream().map(maestro -> {
+            Usuario usuario = buscadorUsuario.igual("id", String.valueOf(maestro.idUsuario())).buscar().get(0);
+            return MaestroData.fromAggregate(maestro, usuario);
+        }).collect(Collectors.toList());
+
+        return new MaestrosData(listaMaestrosData);
+    }
+
+    public MaestrosData buscarHabilidatos() {
+        List<Maestro> listaMaestros = buscadorMaestro
+                .igual("estatus", "1")
+                .ordenarAscendente("noPersonal")
+                .buscar();
+
+        List<MaestroData> listaMaestrosData = listaMaestros.stream().map(maestro -> {
+            Usuario usuario = buscadorUsuario.igual("id", String.valueOf(maestro.idUsuario())).buscar().get(0);
+            return MaestroData.fromAggregate(maestro, usuario);
+        }).collect(Collectors.toList());
+
+        return new MaestrosData(listaMaestrosData);
+    }
+
+    public MaestrosData buscarPorCriterio(String criterio) {
+        List<Maestro> listaMaestros = buscadorMaestro
+                .contiene("noPersonal", criterio).esOpcional()
+                .contiene("nombre", criterio).esOpcional()
+                .contiene("apellidoPaterno", criterio).esOpcional()
+                .contiene("apellidoMaterno", criterio).esOpcional()
+                .ordenarAscendente("noPersonal")
+                .buscar();
+
+        List<MaestroData> listaMaestrosData = listaMaestros.stream().map(maestro -> {
+            Usuario usuario = buscadorUsuario.igual("id", String.valueOf(maestro.idUsuario())).buscar().get(0);
+            return MaestroData.fromAggregate(maestro, usuario);
+        }).collect(Collectors.toList());
+
+        return new MaestrosData(listaMaestrosData);
+    }
+}
