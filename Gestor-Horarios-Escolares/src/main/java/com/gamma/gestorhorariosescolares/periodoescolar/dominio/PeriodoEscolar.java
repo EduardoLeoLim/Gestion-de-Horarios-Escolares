f8d9471649ab46a66bf755fb63c537cf6a98e666
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-04 19:49:04 -0600

    feat(periodoescolar): clase PeriodoEscolar definida

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/periodoescolar/dominio/PeriodoEscolar.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/periodoescolar/dominio/PeriodoEscolar.java
@@ -1,4 +1,54 @@
 package com.gamma.gestorhorariosescolares.periodoescolar.dominio;
 
+import java.util.Date;
+
 public class PeriodoEscolar {
+    private final int id;
+    private final String clave;
+    private final String nombre;
+    private final Date fechaInicio;
+    private final Date fechaFin;
+    private boolean estatus;
+
+    public PeriodoEscolar(int id, String clave, String nombre, Date fechaInicio, Date fechaFin, boolean estatus) {
+        this.id = id;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estatus = estatus;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String clave() {
+        return clave;
+    }
+
+    public String nombre() {
+        return nombre;
+    }
+
+    public Date fechaInicio() {
+        return fechaInicio;
+    }
+
+    public Date fechaFin() {
+        return fechaFin;
+    }
+
+    public void habilitar() {
+        estatus = true;
+    }
+
+    public void deshabilitar() {
+        estatus = false;
+    }
+
+    public boolean estatus() {
+        return estatus;
+    }
+
 }
