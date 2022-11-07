6c1bf5a4afcf75f19183cb87d9e325969f0c4fbc
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-07 14:46:36 -0600

    feat(planestudio): casos de uso registrar plan de estudio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/planestudio/aplicacion/RegistrarPlanEstudio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/planestudio/aplicacion/RegistrarPlanEstudio.java
@@ -1,4 +1,29 @@
 package com.gamma.gestorhorariosescolares.planestudio.aplicacion;
 
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.excepciones.ClaveDuplicadaException;
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.planestudio.aplicacion.registrar.RegistradorPlanEstudio;
+import com.gamma.gestorhorariosescolares.planestudio.dominio.PlanEstudio;
+
+import java.util.List;
+
 public class RegistrarPlanEstudio {
+
+    private final ServicioBuscador<PlanEstudio> buscadorPlanEstudio;
+    private final RegistradorPlanEstudio registradorPlanEstudio;
+
+    public RegistrarPlanEstudio(ServicioBuscador<PlanEstudio> buscadorPlanEstudio,
+                                RegistradorPlanEstudio registradorPlanEstudio) {
+        this.buscadorPlanEstudio = buscadorPlanEstudio;
+        this.registradorPlanEstudio = registradorPlanEstudio;
+    }
+
+    public void registrar(String clave, String nombre) throws ClaveDuplicadaException {
+        List<PlanEstudio> planesEstudios = buscadorPlanEstudio
+                .igual("clave", clave)
+                .buscar();
+        if (!planesEstudios.isEmpty())
+            throw new ClaveDuplicadaException("Ya hay un plan de estudio registrado con la clave " + clave);
+        registradorPlanEstudio.registrar(clave, nombre);
+    }
 }
