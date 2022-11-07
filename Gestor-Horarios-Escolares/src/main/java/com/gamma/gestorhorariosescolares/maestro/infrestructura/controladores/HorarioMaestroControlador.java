44e4ee2f0c2d590211fcbd1c9ded7ede9e3a9f0c
Brandon Lopez <garaeitachi@hotmail.com>
2022-11-07 14:13:01 -0600

    feat(Maestro): menu de horario del maestro

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/infrestructura/controladores/HorarioMaestroControlador.java
@@ -0,0 +1,29 @@
+package com.gamma.gestorhorariosescolares.maestro.infrestructura.controladores;
+
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.stage.CustomStage;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.Temporizador;
+import javafx.stage.Stage;
+
+public class HorarioMaestroControlador  {
+
+    private final Stage stage;
+    private Temporizador temporizadorBusqueda;
+
+    public HorarioMaestroControlador(Stage stage){
+        if (stage == null)
+            throw new NullPointerException();
+
+        this.stage = stage;
+
+
+
+    }
+
+    public void liberarRecursos() {
+        if (temporizadorBusqueda != null)
+            temporizadorBusqueda.cancel();
+    }
+
+
+
+}
