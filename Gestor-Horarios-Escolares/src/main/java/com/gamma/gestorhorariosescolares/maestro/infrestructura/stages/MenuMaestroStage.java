ce8a5994f4d53737b1ef70c2f1fd3d0faf0beefa
Brandon Lopez <garaeitachi@hotmail.com>
2022-11-07 14:01:48 -0600

    feat(Maestro): menu principal del maestro

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/infrestructura/stages/MenuMaestroStage.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/maestro/infrestructura/stages/MenuMaestroStage.java
@@ -1,2 +1,29 @@
-package com.gamma.gestorhorariosescolares.maestro.infrestructura.stages;public class MenuMaestroStage {
+package com.gamma.gestorhorariosescolares.maestro.infrestructura.stages;
+
+import com.gamma.gestorhorariosescolares.App;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.stage.CustomStage;
+import com.gamma.gestorhorariosescolares.maestro.infrestructura.controladores.MenuMaestroControlador;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import org.kordamp.bootstrapfx.BootstrapFX;
+
+import java.io.IOException;
+
+public class MenuMaestroStage extends CustomStage {
+    public MenuMaestroStage() {
+        setTitle("Menu del Maestro ");
+        setResizable(false);
+        try {
+            FXMLLoader root = new FXMLLoader(App.class.getResource("maestro/infrestructura/vistas/MenuMaestro.fxml"));
+            MenuMaestroControlador menuMaestroController = new MenuMaestroControlador(this);
+            root.setController(menuMaestroController);
+            root.load();
+            Parent conntent = root.getRoot();
+            conntent.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+            setContent(conntent);
+        } catch (IOException e) {
+            System.err.println("Error al cargar ventana.");
+            System.err.println(e.getMessage());
+        }
+    }
 }
