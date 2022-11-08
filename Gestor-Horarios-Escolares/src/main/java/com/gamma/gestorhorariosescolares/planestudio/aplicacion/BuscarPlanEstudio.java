983982815ff9f2a7774b7cf200497d1aaa28b6eb
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-08 15:30:25 -0600

    feat(planestudio): casos de uso buscar plan de estudio

---
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/planestudio/aplicacion/BuscarPlanEstudio.java
@@ -0,0 +1,33 @@
+package com.gamma.gestorhorariosescolares.planestudio.aplicacion;
+
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.servicios.ServicioBuscador;
+import com.gamma.gestorhorariosescolares.planestudio.dominio.PlanEstudio;
+
+import java.util.List;
+
+public class BuscarPlanEstudio {
+
+    private final ServicioBuscador<PlanEstudio> buscadorPlanEstudio;
+
+    public BuscarPlanEstudio(ServicioBuscador<PlanEstudio> buscadorPlanEstudio) {
+        this.buscadorPlanEstudio = buscadorPlanEstudio;
+    }
+
+    public PlanesEstudioData buscarTodos() {
+        List<PlanEstudio> planesEstudio = buscadorPlanEstudio
+                .ordenarAscendente("clave")
+                .buscar();
+
+        return new PlanesEstudioData(planesEstudio);
+    }
+
+    public PlanesEstudioData buscarHabilitados() {
+        List<PlanEstudio> planesEstudio = buscadorPlanEstudio
+                .igual("estatus", "1")
+                .ordenarAscendente("clave")
+                .buscar();
+
+        return new PlanesEstudioData(planesEstudio);
+    }
+
+}
--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/planestudio/aplicacion/PlanesEstudioData.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/planestudio/aplicacion/PlanesEstudioData.java
@@ -1,16 +1,20 @@
 package com.gamma.gestorhorariosescolares.planestudio.aplicacion;
 
+import com.gamma.gestorhorariosescolares.planestudio.dominio.PlanEstudio;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlanesEstudioData {
 
     private final List<PlanEstudioData> planesEstudio;
 
-    public PlanesEstudioData(List<PlanEstudioData> planesEstudio) {
-        this.planesEstudio = planesEstudio;
+    public PlanesEstudioData(List<PlanEstudio> planesEstudio) {
+        this.planesEstudio = planesEstudio.stream().map(PlanEstudioData::fromAggregate).collect(Collectors.toList());
     }
 
     public List<PlanEstudioData> planesEstudio() {
         return planesEstudio;
     }
+
 }
