7086b8dc23d23f4610040bf87dfce9e60b23b2ef
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-11 01:07:05 -0600

    feat(materia): catálogo de materias
    Permite buscar, registrar, actualizar materias.

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/materia/infrestructura/controladores/CatalogoMateriasControlador.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/materia/infrestructura/controladores/CatalogoMateriasControlador.java
@@ -1,8 +1,20 @@
 package com.gamma.gestorhorariosescolares.materia.infrestructura.controladores;
 
+import com.gamma.gestorhorariosescolares.compartido.aplicacion.excepciones.RecursoNoEncontradoException;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.conexiones.MySql2oConexiones;
 import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.Temporizador;
+import com.gamma.gestorhorariosescolares.grado.aplicacion.buscar.BuscadorGrado;
+import com.gamma.gestorhorariosescolares.grado.infrestructura.persistencia.MySql2oGradoRepositorio;
+import com.gamma.gestorhorariosescolares.materia.aplicacion.BuscarMaterias;
+import com.gamma.gestorhorariosescolares.materia.aplicacion.GestionarEstatusMateria;
 import com.gamma.gestorhorariosescolares.materia.aplicacion.MateriaData;
 import com.gamma.gestorhorariosescolares.materia.aplicacion.MateriasData;
+import com.gamma.gestorhorariosescolares.materia.aplicacion.actualizar.ActualizadorMateria;
+import com.gamma.gestorhorariosescolares.materia.aplicacion.buscar.BuscadorMateria;
+import com.gamma.gestorhorariosescolares.materia.infrestructura.persistencia.MySql2oMateriaRepositorio;
+import com.gamma.gestorhorariosescolares.materia.infrestructura.stages.FormularioMateriaStage;
+import com.gamma.gestorhorariosescolares.planestudio.aplicacion.buscar.BuscadorPlanEstudio;
+import com.gamma.gestorhorariosescolares.planestudio.infrestructura.persistencia.MySql2oPlanEstudioRepositorio;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -10,6 +22,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 public class CatalogoMateriasControlador {
 
@@ -34,7 +49,7 @@ public class CatalogoMateriasControlador {
     @FXML
     public void initialize() {
 
-        //Configuración de busqueda
+        //Configuración de búsqueda
         temporizadorBusqueda = new Temporizador(1, temporizador -> {
             //Búsqueda de materias
             Platform.runLater(() -> {
@@ -49,8 +64,9 @@ public class CatalogoMateriasControlador {
             temporizadorBusqueda.reiniciar();
         });
 
-        //Configuracion de tabla
+        //Configuración de tabla
         inicializarTabla();
+        buscarMaterias();
     }
 
     private void inicializarTabla() {
@@ -71,6 +87,14 @@ public class CatalogoMateriasControlador {
         TableColumn<MateriaData, String> columnaHorasTeoricas = new TableColumn<>("Horas teóricas");
         columnaHorasTeoricas.setCellValueFactory(ft -> new SimpleStringProperty("" + ft.getValue().horasTeoricas()));
         columnaHorasTeoricas.setMinWidth(150);
+
+        TableColumn<MateriaData, String> columnaPlanGrado = new TableColumn<>("Plan - Grado");
+        columnaPlanGrado.setCellValueFactory(ft -> {
+            MateriaData materiaData = ft.getValue();
+            String texto = materiaData.planEstudio().nombre() + " - " + materiaData.grado().nombre();
+            return new SimpleStringProperty(texto);
+        });
+        columnaPlanGrado.setMinWidth(200);
 
         TableColumn<MateriaData, String> columnaEditar = new TableColumn<>();
         columnaEditar.setMinWidth(80);
@@ -110,6 +134,7 @@ public class CatalogoMateriasControlador {
                 fila.setDisable(false);
 
                 Button botonEstatus = new Button();
+                botonEstatus.setPrefWidth(Double.MAX_VALUE);
                 if (materia.estatus()) {
                     botonEstatus.setText("Deshabilitar");
                     botonEstatus.getStyleClass().addAll("b", "btn-danger");
@@ -137,20 +162,57 @@ public class CatalogoMateriasControlador {
 
         tablaMaterias.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         tablaMaterias.getColumns().addAll(columnaClave, columnaNombre, columnaHorasPracticas, columnaHorasTeoricas,
-                columnaEditar, columnaEstatus);
+                columnaPlanGrado, columnaEditar, columnaEstatus);
         tablaMaterias.setItems(coleccionMaterias);
     }
 
-    private void editarMateria(MateriaData materia) {
+    @FXML
+    private void registrarNuevaMateria() {
+        var formulario = new FormularioMateriaStage();
+        formulario.initOwner(stage);
+        formulario.showAndWait();
+        buscarMaterias();
+    }
 
+    private void editarMateria(MateriaData materia) {
+        var formulario = new FormularioMateriaStage(materia);
+        formulario.initOwner(stage);
+        formulario.showAndWait();
+        buscarMaterias();
     }
 
     private void habilitarMateria(MateriaData materia) {
-
+        cambiarEstatus("habilitar", materia);
     }
 
     private void deshabilitarMateria(MateriaData materia) {
+        cambiarEstatus("deshabilitar", materia);
+    }
 
+    private void cambiarEstatus(String estatus, MateriaData materia) {
+        Sql2o conexion = MySql2oConexiones.getConexionPrimaria();
+
+        try (Connection transaccion = conexion.beginTransaction()) {
+            //Repositorios
+            var materiaRepositorio = new MySql2oMateriaRepositorio(transaccion);
+
+            //Servicios
+            var buscadorMateria = new BuscadorMateria(materiaRepositorio);
+            var actualizadorMateria = new ActualizadorMateria(materiaRepositorio);
+
+            GestionarEstatusMateria gestionarEstatusMateria = new GestionarEstatusMateria(buscadorMateria, actualizadorMateria);
+
+            switch (estatus.toLowerCase()) {
+                case "habilitar" -> gestionarEstatusMateria.habilitarMateria(materia.id());
+                case "deshabilitar" -> gestionarEstatusMateria.deshabilitarMateria(materia.id());
+            }
+
+            transaccion.commit();
+        } catch (RecursoNoEncontradoException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK).showAndWait();
+        } catch (Sql2oException e) {
+            new Alert(Alert.AlertType.ERROR, "Base de datos no disponible.\nIntentalo más tarde", ButtonType.OK).showAndWait();
+        }
     }
 
     private void buscarMaterias() {
@@ -161,7 +223,36 @@ public class CatalogoMateriasControlador {
     }
 
     private void buscarMaterias(String criterioBusqueda) {
-        System.out.println("Busqueda de materias por criterio: '" + criterioBusqueda + "'");
+        if (criterioBusqueda == null)
+            criterioBusqueda = "";
+
+        MateriasData materias;
+        Sql2o conexion = MySql2oConexiones.getConexionPrimaria();
+
+        try (Connection transaccion = conexion.beginTransaction()) {
+            //Repositorios
+            var materiaRepositorio = new MySql2oMateriaRepositorio(transaccion);
+            var gradoRepositorio = new MySql2oGradoRepositorio(transaccion);
+            var planEstudioRepositorio = new MySql2oPlanEstudioRepositorio(transaccion);
+
+            //Servicios
+            var buscadorMateria = new BuscadorMateria(materiaRepositorio);
+            var buscadorGrado = new BuscadorGrado(gradoRepositorio);
+            var buscadorPlanEstudio = new BuscadorPlanEstudio(planEstudioRepositorio);
+
+            BuscarMaterias buscarMaterias = new BuscarMaterias(buscadorMateria, buscadorGrado, buscadorPlanEstudio);
+
+            if (criterioBusqueda.isBlank()) {
+                materias = buscarMaterias.buscarTodos();
+            } else {
+                System.out.println("Busqueda de materias por criterio: '" + criterioBusqueda + "'");
+                materias = buscarMaterias.buscarPorCriterio(criterioBusqueda);
+            }
+
+            cargarMateriaEnTabla(materias);
+        } catch (Sql2oException e) {
+            new Alert(Alert.AlertType.ERROR, "Base de datos no disponible.\nIntentalo más tarde", ButtonType.OK).showAndWait();
+        }
     }
 
     private void cargarMateriaEnTabla(MateriasData listaMaterias) {
