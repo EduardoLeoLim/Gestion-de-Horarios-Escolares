e574ea19030a5edf163c8a8994a5bed4376ab6ec
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-11 00:48:38 -0600

    feat(grado): consultar grados desde el repositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grado/infrestructura/persistencia/MySql2oGradoRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grado/infrestructura/persistencia/MySql2oGradoRepositorio.java
@@ -1,12 +1,17 @@
 package com.gamma.gestorhorariosescolares.grado.infrestructura.persistencia;
 
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.MySqlCriteriaParser;
 import com.gamma.gestorhorariosescolares.grado.dominio.Grado;
 import com.gamma.gestorhorariosescolares.grado.dominio.GradoRepositorio;
 import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.data.Row;
+import org.sql2o.data.Table;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MySql2oGradoRepositorio implements GradoRepositorio {
 
@@ -19,6 +24,30 @@ public class MySql2oGradoRepositorio implements GradoRepositorio {
     @Override
     public List<Grado> buscar(Criteria criterio) {
         List<Grado> grados = new ArrayList<>();
+        String consulta;
+
+        MySqlCriteriaParser conversorCriteria = new MySqlCriteriaParser("grado", criterio);
+
+        consulta = conversorCriteria.generarConsultaSql2o();
+        List<Map<Integer, String>> parametros = conversorCriteria.generarParametrosSql2o();
+
+        Query query = conexion.createQuery(consulta);
+        parametros.forEach(parametro -> {
+            query.addParameter(parametro.get(0), parametro.get(1));
+        });
+        Table tabla = query.executeAndFetchTable();
+        List<Row> filas = tabla.rows();
+        filas.forEach(fila -> {
+            int id = fila.getInteger("id");
+            String clave = fila.getString("clave");
+            String nombre = fila.getString("nombre");
+            int idPlanEstudio = fila.getInteger("idPlanEstudio");
+            boolean estatus = fila.getBoolean("estatus");
+
+            Grado grado = new Grado(id, clave, nombre, idPlanEstudio, estatus);
+            grados.add(grado);
+        });
+
         return grados;
     }
 
