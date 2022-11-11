ff1d611e40ed8bb672a7382e6c88e139accb1b48
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-11 14:18:30 -0600

    feat(grupo): dominio definido

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grupo/dominio/Grupo.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grupo/dominio/Grupo.java
@@ -1,4 +1,55 @@
 package com.gamma.gestorhorariosescolares.grupo.dominio;
 
 public class Grupo {
+    private final int id;
+    private final String clave;
+    private final String nombre;
+    private final int idGrado;
+    private final int[] idMaterias;
+    private final int[] idInscripciones;
+    private final int idPeriodoEscolar;
+
+    public Grupo(int id, String clave, String nombre, int idGrado, int[] idMaterias, int[] idInscripciones, int idPeriodoEscolar) {
+        this.id = id;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.idGrado = idGrado;
+        this.idMaterias = idMaterias;
+        this.idInscripciones = idInscripciones;
+        this.idPeriodoEscolar = idPeriodoEscolar;
+    }
+
+    public Grupo(String clave, String nombre, int idGrado, int[] idMaterias, int[] idInscripciones, int idPeriodoEscolar) {
+        this.idInscripciones = idInscripciones;
+        this.id = 0;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.idGrado = idGrado;
+        this.idMaterias = idMaterias;
+        this.idPeriodoEscolar = idPeriodoEscolar;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public String clave() {
+        return clave;
+    }
+
+    public String nombre() {
+        return nombre;
+    }
+
+    public int idGrado() {
+        return idGrado;
+    }
+
+    public int[] idMaterias() {
+        return idMaterias;
+    }
+
+    public int idPeriodoEscolar() {
+        return idPeriodoEscolar;
+    }
 }
--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grupo/dominio/GrupoRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/grupo/dominio/GrupoRepositorio.java
@@ -1,4 +1,13 @@
 package com.gamma.gestorhorariosescolares.grupo.dominio;
 
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+
+import java.util.List;
+
 public interface GrupoRepositorio {
+    List<Grupo> buscar(Criteria criterio);
+
+    int registrar(Grupo grupo);
+
+    void actualizar(Grupo grupo);
 }
