1a77980d718216b2365a7e2fdf557d001d3ccf65
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-13 21:56:50 -0600

    feat(clase): implementación de ClaseRepositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/clase/infrestructura/persistencia/MySql2oClaseRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/clase/infrestructura/persistencia/MySql2oClaseRepositorio.java
@@ -3,22 +3,73 @@ package com.gamma.gestorhorariosescolares.clase.infrestructura.persistencia;
 import com.gamma.gestorhorariosescolares.clase.dominio.Clase;
 import com.gamma.gestorhorariosescolares.clase.dominio.ClaseRepositorio;
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.MySqlCriteriaParser;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.data.Row;
+import org.sql2o.data.Table;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MySql2oClaseRepositorio implements ClaseRepositorio {
+
+    private final Connection conexion;
+
+    public MySql2oClaseRepositorio(Connection conexion) {
+        this.conexion = conexion;
+    }
+
     @Override
     public List<Clase> buscar(Criteria criterio) {
-        return null;
+        List<Clase> clases = new ArrayList<>();
+        String consulta;
+
+        MySqlCriteriaParser conversorCriteria = new MySqlCriteriaParser("grupo", criterio);
+
+        consulta = conversorCriteria.generarConsultaSql2o();
+        List<Map<Integer, String>> parametros = conversorCriteria.generarParametrosSql2o();
+
+        Query query = conexion.createQuery(consulta);
+        parametros.forEach(parametro -> {
+            query.addParameter(parametro.get(0), parametro.get(1));
+        });
+        Table tabla = query.executeAndFetchTable();
+        List<Row> filas = tabla.rows();
+        filas.forEach(fila -> {
+            Integer id = fila.getInteger("id");
+            Integer idGrupo = fila.getInteger("idGrupo");
+            Integer idMateria = fila.getInteger("idMateria");
+            Integer idMaestro = fila.getInteger("idMaestro");
+
+            Clase clase = new Clase(id, idGrupo, idMateria, idMaestro == null ? 0 : idMaestro);
+            clases.add(clase);
+        });
+
+        return clases;
     }
 
     @Override
     public int registar(Clase clase) {
-        return 0;
+        String consulta = "INSERT INTO clase (idMateria, idGrupo) VALUES (:idMateria, :idGrupo);";
+
+        int idClase = conexion.createQuery(consulta)
+                .addParameter("idGrupo", clase.idGrupo())
+                .addParameter("idMateria", clase.idMateria())
+                .executeUpdate()
+                .getKey(Integer.class);
+
+        return idClase;
     }
 
     @Override
     public void actualizar(Clase clase) {
+        String consulta = "UPDATE clase SET idMaestro = :idMaestro WHERE id = :id;";
 
+        conexion.createQuery(consulta)
+                .addParameter("idMaestro", clase.idMaestro())
+                .addParameter("id", clase.id())
+                .executeUpdate();
     }
 }
