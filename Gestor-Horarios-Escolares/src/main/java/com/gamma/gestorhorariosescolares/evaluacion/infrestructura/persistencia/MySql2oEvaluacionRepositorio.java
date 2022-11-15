a2a17d2b59064c3df92c4649779254ca07b0540d
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-15 00:59:29 -0600

    feat(evaluacion): metodos implementados en repositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/infrestructura/persistencia/MySql2oEvaluacionRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/infrestructura/persistencia/MySql2oEvaluacionRepositorio.java
@@ -1,6 +1,14 @@
 package com.gamma.gestorhorariosescolares.evaluacion.infrestructura.persistencia;
 
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.evaluacion.dominio.Evaluacion;
 import com.gamma.gestorhorariosescolares.evaluacion.dominio.EvaluacionRepositorio;
 
+import java.util.List;
+
 public class MySql2oEvaluacionRepositorio implements EvaluacionRepositorio {
+    @Override
+    public List<Evaluacion> buscar(Criteria criterio) {
+        return null;
+    }
 }
