a9b4cba2e0ee32a63e03a6a50806de909cbecb85
Brandon Lopez <garaeitachi@hotmail.com>
2022-11-14 23:33:48 -0600

    refactor(evaluacion): se agrega el metodo abstracto buscar() al EvaluacionRepositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/dominio/EvaluacionRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/dominio/EvaluacionRepositorio.java
@@ -1,4 +1,11 @@
 package com.gamma.gestorhorariosescolares.evaluacion.dominio;
 
+import com.gamma.gestorhorariosescolares.administrador.dominio.Administrador;
+import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+
+import java.util.List;
+
 public interface EvaluacionRepositorio {
+
+    List<Evaluacion> buscar(Criteria criterio);
 }
