f822f845fad003de2552d8a73dafae5218a67051
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-16 15:31:45 -0600

    feat(inscripcion): método "buscar" de implementacion de repositorio definido

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/inscripcion/infrestructura/persistencia/MySql2oInscripcionRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/inscripcion/infrestructura/persistencia/MySql2oInscripcionRepositorio.java
@@ -1,15 +1,53 @@
 package com.gamma.gestorhorariosescolares.inscripcion.infrestructura.persistencia;
 
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
+import com.gamma.gestorhorariosescolares.compartido.infrestructura.utilerias.MySqlCriteriaParser;
 import com.gamma.gestorhorariosescolares.inscripcion.dominio.Inscripcion;
 import com.gamma.gestorhorariosescolares.inscripcion.dominio.InscripcionRepositorio;
+import org.sql2o.Connection;
+import org.sql2o.Query;
+import org.sql2o.data.Row;
+import org.sql2o.data.Table;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class MySql2oInscripcionRepositorio implements InscripcionRepositorio {
+
+    private final Connection conexion;
+
+    public MySql2oInscripcionRepositorio(Connection conexion) {
+        this.conexion = conexion;
+    }
+
     @Override
     public List<Inscripcion> buscar(Criteria criterio) {
-        return null;
+        List<Inscripcion> inscripciones = new ArrayList<>();
+        String consulta;
+
+        MySqlCriteriaParser conversorCriteria = new MySqlCriteriaParser("inscripcion", criterio);
+
+        consulta = conversorCriteria.generarConsultaSql2o();
+        List<Map<Integer, String>> parametros = conversorCriteria.generarParametrosSql2o();
+
+        Query query = conexion.createQuery(consulta);
+        parametros.forEach(parametro -> {
+            query.addParameter(parametro.get(0), parametro.get(1));
+        });
+        Table tabla = query.executeAndFetchTable();
+        List<Row> filas = tabla.rows();
+        filas.forEach(fila -> {
+            Integer id = fila.getInteger("id");
+            Date fechaRegistro = fila.getDate("idfechaRegistro");
+            Integer idGrado = fila.getInteger("idGrado");
+            Integer idPeriodoEscolar = fila.getInteger("idPeriodoEscolar");
+            Integer idAlumno = fila.getInteger("idAlumno");
+
+            inscripciones.add(new Inscripcion(id, fechaRegistro, idGrado, idPeriodoEscolar, idAlumno));
+        });
+        return inscripciones;
     }
 
     @Override
