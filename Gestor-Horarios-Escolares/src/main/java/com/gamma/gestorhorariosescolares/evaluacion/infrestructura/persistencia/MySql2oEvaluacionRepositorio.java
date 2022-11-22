3944a3a5ba81c49551e86f8cc09174b32c73fc1a
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-11-21 21:41:01 -0600

    feat(evaluacion): conexión de sql2o agregada a repositorio

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/infrestructura/persistencia/MySql2oEvaluacionRepositorio.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/infrestructura/persistencia/MySql2oEvaluacionRepositorio.java
@@ -3,10 +3,18 @@ package com.gamma.gestorhorariosescolares.evaluacion.infrestructura.persistencia
 import com.gamma.gestorhorariosescolares.compartido.dominio.criterio.Criteria;
 import com.gamma.gestorhorariosescolares.evaluacion.dominio.Evaluacion;
 import com.gamma.gestorhorariosescolares.evaluacion.dominio.EvaluacionRepositorio;
+import org.sql2o.Connection;
 
 import java.util.List;
 
 public class MySql2oEvaluacionRepositorio implements EvaluacionRepositorio {
+
+    private final Connection connection;
+
+    public MySql2oEvaluacionRepositorio(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public List<Evaluacion> buscar(Criteria criterio) {
         return null;
