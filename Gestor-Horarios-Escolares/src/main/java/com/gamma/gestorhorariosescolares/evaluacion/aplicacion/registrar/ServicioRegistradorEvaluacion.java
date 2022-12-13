9fdef0aa716aa14bf699d3809ce595086fba106e
Brandon Lopez <garaeitachi@hotmail.com>
2022-12-12 19:36:16 -0600

    Refactor(Evaluacion): se agrega idInscripcion

--- Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/aplicacion/registrar/ServicioRegistradorEvaluacion.java
+++ Gestor-Horarios-Escolares/src/main/java/com/gamma/gestorhorariosescolares/evaluacion/aplicacion/registrar/ServicioRegistradorEvaluacion.java
@@ -2,5 +2,5 @@ package com.gamma.gestorhorariosescolares.evaluacion.aplicacion.registrar;
 
 public interface ServicioRegistradorEvaluacion {
 
-    int registrar(String calificacion, String tipo, Integer idMateria, Integer idAlumno);
+    int registrar(String calificacion, String tipo, Integer idMateria, Integer idAlumno, Integer idInscripcion);
 }
